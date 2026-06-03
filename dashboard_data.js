6fa0df94693153cd9111ae86db5693dd51f3c691
EBL-DASHBOARD <samson@ebl.sg>
2026-06-03 13:03:40 +0800

    dashboard update 2026-06-03T05:03:39.719Z

--- dashboard_data.js
+++ dashboard_data.js
@@ -1,11 +1,139 @@
 window.DASHBOARD_DATA = {
-  "last_updated": "2026-06-03T05:00:35.507Z",
+  "last_updated": "2026-06-03T05:03:39.065Z",
   "groups": [
     {
       "id": "EBL6",
       "name": "EBL-6 (BK/PMI)",
       "total_locations": 42,
       "done": 20,
+      "outlets_done": [
+        "JP",
+        "KRC",
+        "CL2",
+        "USQ",
+        "CCKS",
+        "BNV",
+        "ARC",
+        "ART",
+        "HV2",
+        "SHP",
+        "TRM",
+        "BQ",
+        "TIO",
+        "DV",
+        "KIT",
+        "CT",
+        "VIVO",
+        "PS",
+        "WSM",
+        "KLP"
+      ],
+      "outlets_outstanding": [
+        "MSQ",
+        "TNC2",
+        "TPM",
+        "KNX",
+        "LOY",
+        "EPT",
+        "CCP",
+        "PAL",
+        "AP4",
+        "BOB",
+        "BHT",
+        "NEX",
+        "AP1",
+        "R3",
+        "R1",
+        "R4",
+        "AP2",
+        "KTB",
+        "NTP",
+        "WIS",
+        "ACV",
+        "WWP"
+      ],
+      "done_detail": {
+        "JP": {
+          "by": "Samy Kumar Meena",
+          "time": "09:44"
+        },
+        "KRC": {
+          "by": "GK",
+          "time": "09:44"
+        },
+        "CL2": {
+          "by": "Krishnamiyako",
+          "time": "09:44"
+        },
+        "USQ": {
+          "by": "Sasi",
+          "time": "09:45"
+        },
+        "CCKS": {
+          "by": "Samy Kumar Meena",
+          "time": "09:48"
+        },
+        "BNV": {
+          "by": "GK",
+          "time": "10:17"
+        },
+        "ARC": {
+          "by": "Krishnamiyako",
+          "time": "10:29"
+        },
+        "ART": {
+          "by": "Krishnamiyako",
+          "time": "10:29"
+        },
+        "HV2": {
+          "by": "Samy Kumar Meena",
+          "time": "10:31"
+        },
+        "SHP": {
+          "by": "Sasi",
+          "time": "10:55"
+        },
+        "TRM": {
+          "by": "Samy Kumar Meena",
+          "time": "11:03"
+        },
+        "BQ": {
+          "by": "GK",
+          "time": "11:06"
+        },
+        "TIO": {
+          "by": "Krishnamiyako",
+          "time": "11:27"
+        },
+        "DV": {
+          "by": "Samy Kumar Meena",
+          "time": "11:32"
+        },
+        "KIT": {
+          "by": "Sasi",
+          "time": "11:33"
+        },
+        "CT": {
+          "by": "GK",
+          "time": "11:54"
+        },
+        "VIVO": {
+          "by": "Krishnamiyako",
+          "time": "12:09"
+        },
+        "PS": {
+          "by": "GK",
+          "time": "12:11"
+        },
+        "WSM": {
+          "by": "Samy Kumar Meena",
+          "time": "12:20"
+        },
+        "KLP": {
+          "by": "Sasi",
+          "time": "12:23"
+        }
+      },
       "customers": null
     }
   ]
